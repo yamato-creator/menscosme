5ee4c620b0f2578f04f3321bac8d4ee5ce2b02f4
yamato-creator <dydydydydydy5656@gmail.com>
2021-03-24 10:01:08 +0900

    fix_tweets_edit

--- resources/views/tweets/edit.blade.php
+++ resources/views/tweets/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Update</div>
+                <div class="card-header">ツイートを編集</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('tweets.update', [$tweets -> id]) }}">
